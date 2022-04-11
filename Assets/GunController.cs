f1e6e2d1e094fe518fb91900de2480c1380b76d7
Umar <umarhyattoffice@gmail.com>
2022-04-11 09:56:45 +0500

    add gun zoom

--- Assets/GunController.cs
+++ Assets/GunController.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using Cinemachine;
 public class GunController : MonoBehaviour
 {
+    public Slider zoomValue;
     public float range = 10;
     public float damage = 50;
+    public CinemachineVirtualCamera CMcamera;
     RaycastHit hit;
     private void Start()
     {
         Cursor.lockState=CursorLockMode.Locked;
     }
+    public void ScopeZoom()
+    {
+        CMcamera.m_Lens.FieldOfView=zoomValue.value;
+    }
     private void Update()
     {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            zoomValue.value-=Time.deltaTime*15;
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            zoomValue.value+=Time.deltaTime*15;
+        }
         Debug.DrawRay(transform.position, transform.forward * range, Color.blue);
         if (Input.GetMouseButtonDown(0))
         {
